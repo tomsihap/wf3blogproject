1f1f9890dd4c32bb82f0d9c6c1427137c89fda64
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2020-05-29 17:08:11 +0200

    category + relation (création et affichage de categorie)

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -2,33 +2,25 @@
 
 namespace App\Form;
 
-use App\Entity\Article;
 use App\Entity\Category;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ArticleType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title')
-            ->add('content')
-            ->add('created_at')
-            ->add('short_content')
-            ->add('category', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'title'
-            ])
+            ->add('description')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Article::class,
+            'data_class' => Category::class,
         ]);
     }
 }
